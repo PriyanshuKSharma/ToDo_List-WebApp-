db01e56ff3c1c8f8e628c3a992859e458adef5d5
PriyanshuKSharma <priyanshu17ks@gmail.com>
2024-04-09 13:51:54 +0530

    Added env file(for virtual environment interpretor), static directory(to store files like pics, videos, audios) and template directory(for writing frontend)

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:////todo.db"
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    return 'Hello, World!!'
+
+
+@app.route('/products')
+def products():
+    return 'this the product page'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+ 
